a55bb58eaeff5693bf20ddf2275e4d2ca748baa2
chandanikothiya <kothiyachandani34@gmail.com>
2026-02-13 18:36:43 +0530

    11_02_26_client_to_clinet_msg

--- src/container/Chat/Chat.jsx
+++ src/container/Chat/Chat.jsx
@@ -1,23 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { io } from 'socket.io-client';
-
+import MyTextField from '../../admin/components/MyTextField/MyTextField';
+import { Button, TextField } from '@mui/material';
 
 function Chat(props) {
+
+    const [formdata, setFormData] = useState({ socketid: '', message: '' })
 
     const socket = io('http://localhost:4000');
 
     socket.on('connect', () => {
-        console.log("soketID:",socket.id)
+        console.log("soketID:", socket.id)
     });
 
-    socket.on('welcome',(message) => {
+    socket.on('welcome', (message) => {
+        console.log(message);
+    })
+
+    function handleChange(e) {
+        //console.log(e.target.name,e.target.value,{[e.target.name]:e.target.value}) //use [] beacuse 1 change function use for multiple input
+        setFormData({ ...formdata, [e.target.name]: e.target.value })
+    }
+
+    function handlesubmit() {
+        event.preventDefault();
+        console.log("ok")
+        //console.log(formdata)
+       socket.emit("sendmsg",{id:formdata.socketid,message:formdata.message})
+    }
+
+    socket.on('recivemessage', (message) => {
         console.log(message);
     })
 
     return (
-        <div>
-            
-        </div>
+
+        <form
+            style={{ display: 'flex', flexDirection: 'column', width: '400px', padding: '30px', boxShadow: '0 0 4px 0 rgba(0,0,0,0.25)', margin: '20px auto 0 auto', borderRadius: '5px' }}
+            onSubmit={handlesubmit}
+        >
+
+            <h3>Send Message</h3>
+
+            <TextField
+                variant='standard'
+                label="Socket ID"
+                name='socketid'
+                id='socketid'
+                value={formdata.socketid}
+                onChange={handleChange}
+            />
+
+            <TextField
+                variant='standard'
+                label="Enter Message"
+                name='message'
+                id='message'
+                multiline
+                rows={4}
+                style={{ marginTop: '20px' }}
+                value={formdata.message}
+                onChange={handleChange}
+            />
+
+            <Button type='submit' variant='contained' style={{ marginTop: '30px' }}>Send Message</Button>
+        </form>
+
     );
 }
 
