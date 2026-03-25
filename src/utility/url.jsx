fe12b494adf673b4cb9bc651c1b35ebb0a1fa1bc
chandanikothiya <kothiyachandani34@gmail.com>
2026-03-25 18:24:56 +0530

    dd

--- src/utility/url.jsx
+++ src/utility/url.jsx
@@ -1,5 +1,5 @@
-export const BASE_URL = "http://localhost:8080/api/v1/"
-export const IMG_URL = "http://localhost:8080/"
+export const BASE_URL = "https://backend-chi-rosy-95.vercel.app"
+export const IMG_URL = "https://backend-chi-rosy-95.vercel.app"
 
 //chnge url when live
 // const BASE_URL = "http://localhost:8080/api/v1"
