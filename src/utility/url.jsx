bd42de840d3a04f5dc5e47556dee5afa71e12e0c
chandanikothiya <kothiyachandani34@gmail.com>
2026-03-25 18:45:56 +0530

    dd

--- src/utility/url.jsx
+++ src/utility/url.jsx
@@ -1,5 +1,5 @@
-export const BASE_URL = "https://elevate-knowledge-silk.vercel.app/"
-export const IMG_URL = "https://elevate-knowledge-silk.vercel.app/"
+export const BASE_URL = "https://backend-chi-rosy-95.vercel.app/"
+export const IMG_URL = "https://backend-chi-rosy-95.vercel.app/"
 
 //chnge url when live
 // const BASE_URL = "http://localhost:8080/api/v1"
