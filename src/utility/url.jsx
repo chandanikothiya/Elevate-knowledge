8180774335c765ea10e244370fee0dbf25918908
chandanikothiya <kothiyachandani34@gmail.com>
2026-03-30 17:09:21 +0530

    dd

--- src/utility/url.jsx
+++ src/utility/url.jsx
@@ -1,8 +1,8 @@
-// export const BASE_URL = "https://backend-chi-rosy-95.vercel.app/api/v1/"
-// export const IMG_URL = "https://backend-chi-rosy-95.vercel.app/"
+export const BASE_URL = "https://backend-chi-rosy-95.vercel.app/api/v1/"
+export const IMG_URL = "https://backend-chi-rosy-95.vercel.app/"
 
-export const BASE_URL = "http://localhost:8080/api/v1/"
-export const IMG_URL = "http://localhost:8080/"
+// export const BASE_URL = "http://localhost:8080/api/v1/"
+// export const IMG_URL = "http://localhost:8080/"
 
 //chnge url when live
 // const BASE_URL = "http://localhost:8080/api/v1"
